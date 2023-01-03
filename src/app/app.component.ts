9a4534f76a1466e69ee568ca9d06ba62bb36be11
HoDangSan <38898495+HoDangSan@users.noreply.github.com>
2023-01-03 22:44:37 +0700

    3 files modified, 2 added, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,19 @@ import { from } from 'rxjs';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  name = 'Angular ' + VERSION.major;
+  // array: { value: }[] = [
 
-
-  array: { value: }[] = [
-
-  ]
+  // ]
 
   ngOnInit() {
     this.init();
   }
 
   init() {
-    from([1, 2, 3])
-      .pipe(last)
-      .subscribe(console.log, null, () => console.log('complete'));
+    // from([1, 2, 3])
+    //   .pipe(last)
+    //   .subscribe(console.log, null, () => console.log('complete'));
   }
 }
---
+++ src/app/modules/customer/customer.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
